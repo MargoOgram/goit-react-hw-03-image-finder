c9af69f1489a58739efb0c7af958c37b9dd20efc
MargoOgram <margo_14@ukr.net>
2023-04-09 23:01:22 +0300

    add

---
+++ src/components/Services/Api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// const API_KEY = '31369835-3fa2149de848f1cb6ffd48aa5';
+// const BASE_URL = 'https://pixabay.com';
+
+const BASE_URL = 'https://pixabay.com';
+const API_KEY = '33021124-e3386d60ac1ab8b465ffa3402';
+
+export const fetchData = (query, page, perPage) => {
+  return axios
+    .get(
+      `${BASE_URL}/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+    )
+    .then(response => response.data);
+};
